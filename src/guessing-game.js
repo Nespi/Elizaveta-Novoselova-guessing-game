a566587b5f972c13b653e4e75f2be9a9fd138096
Liza <lizayablonska@gmail.com>
2017-02-05 01:03:59 +0300

    not done yet

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this.minRange = 0;
+        this.maxRange = 0;
+    }
 
     setRange(min, max) {
-
+        this.minRange = min;
+        this.maxRange = max;
+        console.log(this.minRange + ":" + this.maxRange);
     }
 
     guess() {
-
+        console.log((this.maxRange - this.minRange)/2^0 + this.minRange);
+        return ((this.maxRange - this.minRange)/2^0 + this.minRange);
     }
 
     lower() {
-
+        console.log("maxRange=" + this.guess());
+        this.maxRange = this.guess();
     }
 
     greater() {
-
+        console.log("minRange=" + this.guess());
+        this.minRange = this.guess();
     }
 }
 
